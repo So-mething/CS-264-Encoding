389c3d6ee309ddc5d55fc1ea57d970237f4d58a1
Rebecca Irlin <rebecca.irlin@my.liu.edu>
2026-02-08 11:55:47 -0500

    Update morsecode.py

--- src/morsecode.py
+++ src/morsecode.py
@@ -1,12 +1,13 @@
 Morse_Code_Dictionary = {'a': '.-','b': '-...','c': '-.-.','d': '-..','e': '.','f': '..-.','g': '--.','h': '....','i': '..','j': '.---','k': '-.--','l': '.-..','m': '--','n': '-.','o': '---','p': '.--.','q': '--.-','r': '.-.-','s': '...','t': '-','u': '..-','v':'...-','w': '.--','x': '-..-','y': '-.--','z': '--..', ' ': '/'} 
 
 def words_to_morse(message): 
-    morse = [] 
+    morse_message = [] 
 
-for char in message.lower(): 
-    if char in Morse_Code_Dictionary: 
-        morse_message.append(Morse_Code_Dictionary[char]) 
-    else:
-        pass 
-        
-return " ". join(morse_message)
+    for char in message.lower(): 
+        if char in Morse_Code_Dictionary: 
+            morse_message.append(Morse_Code_Dictionary[char])       
+    return " ". join(morse_message)
+
+user_word = input("Word: ")
+morse = words_to_morse(user_word) 
+print("Morse Code: ", morse)  
