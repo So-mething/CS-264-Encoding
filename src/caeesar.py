a3e65934ad93b52aa4dfd80dcf1d52990cb7f2e9
Muba-S <mubashir.shehun@my.liu.edu>
2026-03-02 16:47:17 -0500

    Update caeesar.py
    Changed the "print(output_text)" to "return output_text" and remove the loops + inputs to work with the frontend.

--- src/caeesar.py
+++ src/caeesar.py
@@ -12,18 +12,7 @@ def caesar(original_text, shift_amount, encode_or_decode):
             shifted_position = alphabet.index(letter) + shift_amount
             shifted_position %= len(alphabet)
             output_text += alphabet[shifted_position]
-    print(f"Here is the {encode_or_decode}d result: {output_text}")
+    # print(f"Here is the {encode_or_decode}d result: {output_text}")
+    return output_text
 
 should_continue = True
-
-while should_continue == True:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number (when encoding, positive = forward and negative = backwards):\n"))
-
-    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
-    restart = input("Type 'yes' if you want to encode/decode something else. Otherwise, type 'no.'\n").lower()
-    if restart == "no":
-        should_continue = False
-        print("You have concluded the process.")
