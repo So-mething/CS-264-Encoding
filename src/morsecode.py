83e8b4d1a289f51541fa020da7e13c594e90df75
Rebecca Irlin <rebecca.irlin@my.liu.edu>
2026-03-16 16:27:43 -0400

    Update morsecode.py

--- src/morsecode.py
+++ src/morsecode.py
@@ -8,7 +8,7 @@ def morse_to_words(morse_message):
     for code in morse_message.split(): 
         if code in Reverse_Morse_Dictionary:
             words.append(Reverse_Morse_Dictionary[code])
-    return "".join(words)
+    return " ".join(words)
 
 def words_to_morse(message): 
     morse_message = [] 
@@ -16,4 +16,4 @@ def words_to_morse(message):
     for char in message.lower(): 
         if char in Morse_Code_Dictionary: 
             morse_message.append(Morse_Code_Dictionary[char])       
-    return "". join(morse_message)
+    return " ". join(morse_message)
