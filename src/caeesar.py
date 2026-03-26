84261e3aa54a4c0c4edf70812b5adcc0f317ca75
Rebecca Irlin <rebecca.irlin@my.liu.edu>
2026-03-26 14:39:57 -0400

    Made guard rails for Ceasar.py

--- src/caeesar.py
+++ src/caeesar.py
@@ -1,10 +1,23 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(original_text, shift_amount, encode_or_decode):
-    output_text = ""
+    if encode_or_decode not in ("encode", "decode"): 
+        raise ValueError("Choose 'encode' or 'decode'")
+        
+    if not isinstance(shift_amount, int):
+        raise TypeError("The shift amount must be an integer")
+        
+    shift_amount = shift_amount % len(alphabet)
 
     if encode_or_decode == "decode":
         shift_amount *= -1
+
+    if not isinstance(original_text, str):
+        raise TypeError("The original text must be a string")
+
+    original_text = original_text.lower()
+    output_text = ""
+
     for letter in original_text:
         if letter not in alphabet:
             output_text += letter
@@ -15,4 +28,4 @@ def caesar(original_text, shift_amount, encode_or_decode):
     # print(f"Here is the {encode_or_decode}d result: {output_text}")
     return output_text
 
-should_continue = True
+
